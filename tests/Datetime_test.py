c5ce3774d724362da466f438d2143bfd7918985b
Dinesh Kumar Rama <52581367+dk18234@users.noreply.github.com>
2023-03-07 15:22:13 -0600

    Update Datetime_test.py

--- tests/Datetime_test.py
+++ tests/Datetime_test.py
@@ -15,16 +15,16 @@
 #             project0.GPTmain.download_pdf(url)
 
 import os
-import project0.GPTmain
+import project0.main
 import pytest
 
 def test_download_pdf_success():
     url = "https://www.normanok.gov/sites/default/files/documents/2022-10/2022-10-06_daily_incident_summary.pdf"
-    filename = project0.GPTmain.download_pdf(url)
+    filename = project0.main.download_pdf(url)
     assert os.path.exists(filename)
     os.remove(filename)
 
 def test_download_pdf_failure():
     url = "https://www.normanok.gov/sites/default/files/documents/2022-10/2022-10-06_daily_incident_summary_invalid_url.pdf"
     with pytest.raises(Exception):
-        project0.GPTmain.download_pdf(url)
+        project0.main.download_pdf(url)
