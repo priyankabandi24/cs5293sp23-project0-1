d533992f333442c085e60455af727e9bf4d70971
priyankabandi24 <124020159+priyankabandi24@users.noreply.github.com>
2023-03-07 23:29:30 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
 	name='project0',
 	version='1.0',
-	author='Rama Dinesh Kumar',
-	author_email='Dinesh.Kumar.Rama-1@ou.edu',
+	author='Priyanka Bandi',
+	author_email='Priyanka.Bandi-1@ou.edu',
 	packages=find_packages(exclude=('tests', 'docs')),
 	setup_requires=['pytest-runner'],
 	tests_require=['pytest']	
