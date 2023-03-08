57cc7105c98e51d05a1b73be8d71df1c05291f83
Dinesh Kumar Rama <52581367+dk18234@users.noreply.github.com>
2023-03-07 23:28:41 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
 	name='project0',
 	version='1.0',
-	author='You Name',
-	author_email='your ou email',
+	author='Rama Dinesh Kumar',
+	author_email='Dinesh.Kumar.Rama-1@ou.edu',
 	packages=find_packages(exclude=('tests', 'docs')),
 	setup_requires=['pytest-runner'],
 	tests_require=['pytest']	
